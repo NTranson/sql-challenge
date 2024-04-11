c3aa1d4d56e2b5c75151d07a48680ba779b4359d
Nathan Transon <transonnathan@gmail.com>
2024-04-11 12:40:59 -0700

    Schema correction

--- EmployeeSQL/schema.sql
+++ EmployeeSQL/schema.sql
@@ -3,58 +3,91 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE `contacts` (
-    `contact_id` INT  NOT NULL ,
-    `first_name` VARCHAR  NOT NULL ,
-    `last_name` VARCHAR  NOT NULL ,
-    `email` VARCHAR  NOT NULL ,
-    PRIMARY KEY (
-        `contact_id`
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [departments] (
+    [dept_no] VARCHAR  NOT NULL ,
+    [dept_name] VARCHAR  NOT NULL ,
+    CONSTRAINT [PK_departments] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC
     )
-);
+)
 
-CREATE TABLE `category` (
-    `category_id` VARCHAR  NOT NULL ,
-    `category` VARCHAR  NOT NULL ,
-    PRIMARY KEY (
-        `category_id`
+CREATE TABLE [dept_emp] (
+    [emp_no] INTEGER  NOT NULL ,
+    [dept_no] CARCHAR  NOT NULL ,
+    CONSTRAINT [PK_dept_emp] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC,[dept_no] ASC
     )
-);
+)
 
-CREATE TABLE `subcategory` (
-    `subcategory_id` VARCHAR  NOT NULL ,
-    `subcategory` VARCHAR  NOT NULL ,
-    PRIMARY KEY (
-        `subcategory_id`
+CREATE TABLE [dept_manager] (
+    [dept_no] VARCHAR  NOT NULL ,
+    [emp_no] VARCHAR  NOT NULL ,
+    CONSTRAINT [PK_dept_manager] PRIMARY KEY CLUSTERED (
+        [dept_no] ASC,[emp_no] ASC
     )
-);
+)
 
-CREATE TABLE `campaign` (
-    `cf_id` INT  NOT NULL ,
-    `contact_id` INT  NOT NULL ,
-    `company_name` VARCHAR  NOT NULL ,
-    `description` VARCHAR  NOT NULL ,
-    `goal` FLOAT  NOT NULL ,
-    `pleged` FLOAT  NOT NULL ,
-    `outcome` VARCHAR  NOT NULL ,
-    `backers_count` INT  NOT NULL ,
-    `country` VARCHAR(2)  NOT NULL ,
-    `currency` VARCHAR(3)  NOT NULL ,
-    `launched_date` DATE  NOT NULL ,
-    `end_date` DATE  NOT NULL ,
-    `category_id` VARCHAR  NOT NULL ,
-    `subcategory_id` VARCHAR  NOT NULL ,
-    PRIMARY KEY (
-        `cf_id`
+CREATE TABLE [employees] (
+    [emp_no] INTEGER  NOT NULL ,
+    [emp_title_id] VARCHAR  NOT NULL ,
+    [birth_date] DATE  NOT NULL ,
+    [first_name] VARCHAR  NOT NULL ,
+    [last_name] VARCHAR  NOT NULL ,
+    [sex] VARCHAR  NOT NULL ,
+    [hire_date] DATE  NOT NULL ,
+    CONSTRAINT [PK_employees] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
     )
-);
+)
 
-ALTER TABLE `campaign` ADD CONSTRAINT `fk_campaign_contact_id` FOREIGN KEY(`contact_id`)
-REFERENCES `contacts` (`contact_id`);
+CREATE TABLE [salaries] (
+    [emp_no] INTEGER  NOT NULL ,
+    [salary] INTEGER  NOT NULL ,
+    CONSTRAINT [PK_salaries] PRIMARY KEY CLUSTERED (
+        [emp_no] ASC
+    )
+)
 
-ALTER TABLE `campaign` ADD CONSTRAINT `fk_campaign_category_id` FOREIGN KEY(`category_id`)
-REFERENCES `category` (`category_id`);
+CREATE TABLE [titles] (
+    [title_id] VARCHAR  NOT NULL ,
+    [title] VARCHAR  NOT NULL ,
+    CONSTRAINT [PK_titles] PRIMARY KEY CLUSTERED (
+        [title_id] ASC
+    )
+)
 
-ALTER TABLE `campaign` ADD CONSTRAINT `fk_campaign_subcategory_id` FOREIGN KEY(`subcategory_id`)
-REFERENCES `subcategory` (`subcategory_id`);
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
 
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_emp_no]
+
+ALTER TABLE [dept_emp] WITH CHECK ADD CONSTRAINT [FK_dept_emp_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [departments] ([dept_no])
+
+ALTER TABLE [dept_emp] CHECK CONSTRAINT [FK_dept_emp_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_dept_no] FOREIGN KEY([dept_no])
+REFERENCES [departments] ([dept_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_dept_no]
+
+ALTER TABLE [dept_manager] WITH CHECK ADD CONSTRAINT [FK_dept_manager_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [employees] ([emp_no])
+
+ALTER TABLE [dept_manager] CHECK CONSTRAINT [FK_dept_manager_emp_no]
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_no] FOREIGN KEY([emp_no])
+REFERENCES [salaries] ([emp_no])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_no]
+
+ALTER TABLE [employees] WITH CHECK ADD CONSTRAINT [FK_employees_emp_title_id] FOREIGN KEY([emp_title_id])
+REFERENCES [titles] ([title_id])
+
+ALTER TABLE [employees] CHECK CONSTRAINT [FK_employees_emp_title_id]
+
+COMMIT TRANSACTION QUICKDBD
